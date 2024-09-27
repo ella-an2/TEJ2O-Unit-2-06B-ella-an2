09f66659058d7f712911ac5351668c521b5978d5
ella-an2 <ella.an@mths.ca>
2024-09-27 10:54:51 -0400

    wrote the program

--- main.ts
+++ main.ts
@@ -4,5 +4,16 @@
  * Created on: Sep 2024
  * This program lights up one LED
 */
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16,0)
+basic.showIcon(IconNames.Happy)
 
+input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
 
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
