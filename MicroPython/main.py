6c83f19afc2022a4d325b51aa6b2c11718a4cab2
ella-an2 <ella.an@mths.ca>
2024-09-27 15:12:28 +0000

    programmed the program

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ella
+Created on: Sep 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
